ceae898593f584535c323213c12771df9c83239f
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-11-21 20:13:03 +0600

    scroll bar added

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -7,6 +7,12 @@ const Home = () => {
       <div className="bg-primary lg:h-screen flex items-center justify-evenly overflow-hidden ">
         <Title />
         <Gallery />
+        <div>
+          <h3 className="text-highlight">
+            S <br />c <br />r <br />o <br /> l <br /> l <br />D <br /> o <br />{" "}
+            w <br /> n
+          </h3>
+        </div>
       </div>
     </>
   );
